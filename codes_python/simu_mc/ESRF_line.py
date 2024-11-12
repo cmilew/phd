144ddc659bb16558e34600d1c1e73f853158e826
cmilew <milewskicandice@gmail.com>
2024-11-12 14:07:32 +0100

    last commit before changing collim structure

--- codes_python/simu_mc/ESRF_line.py
+++ codes_python/simu_mc/ESRF_line.py
@@ -30,10 +30,11 @@ def create_collimator(
     col_box_name,
     col_name,
     slit_name,
-    material,
+    col_material,
     translation,
     box_dim,
     slit_dim,
+    slit_material,
     cut_col,
     cut_slit,
 ):
@@ -42,12 +43,12 @@ def create_collimator(
 
     # Create box
     col_box = create_box_vol(
-        col_box_name, mother_volume, box_dim, translation, material, [1, 0, 0, 1]
+        col_box_name, mother_volume, box_dim, translation, col_material, [1, 0, 0, 1]
     )
 
     # Create slit
     slit = create_box_vol(
-        slit_name, col_box_name, slit_dim, [0, 0, 0], "G4_AIR", [0, 0, 0, 0]
+        slit_name, col_box_name, slit_dim, [0, 0, 0], slit_material, [0, 0, 0, 0]
     )
 
     # Create collimator = box - slit
@@ -138,21 +139,21 @@ def run_simulation(n_part):
     mm = gate.g4_units.mm
     um = gate.g4_units.um
     eV = gate.g4_units.eV
-    keV = gate.g4_units.keV
+    MeV = gate.g4_units.MeV
     deg = gate.g4_units.deg
 
     # Simulation parameters
     N_PARTICLES = n_part
-    N_THREADS = 18
+    N_THREADS = 1
+    unit_spec_file = eV
     sleep_time = False  # only for parallel simulation on CC
     physics_list = "G4EmLivermorePolarizedPhysics"
     phsp = True
-    visu = False
+    visu = True
     kill_act = False
 
     # Beam dimensions defined at sec col
-    # beam_width = 35e-3 * m  # beam width for step phantom setup
-    beam_width = 60e-6 * m  # 1 mb
+    beam_width = 35e-3 * m  # beam width for step phantom setup
     beam_height = 795e-6 * m
 
     # Dist between source and collimators
@@ -200,7 +201,7 @@ def run_simulation(n_part):
     vac_sec.size = [1 * m, 1 * m, 37.1 * m]
     vac_sec.translation = [0, 0, 2.45 * m]
     vac_sec.material = "Vacuum"
-    vac_sec.color = [0, 0.5, 0, 0.5]
+    vac_sec.color = [0, 0, 0, 0]
     sim.physics_manager.set_production_cut("vacuum_sec", "all", 1 * m)
 
     # Point source defined at wiggler
@@ -213,90 +214,208 @@ def run_simulation(n_part):
     source.direction.type = "iso"
     source.direction.theta = [0 * deg, beam_div]
     source.direction.phi = [0 * deg, 360 * deg]
-    source.energy.type = "mono"
-    source.energy.mono = 123 * keV
-    # source.energy.type = "spectrum_lines"
-    # spectrum = np.loadtxt(
-    #     os.path.join(os.path.dirname(__file__), "data/ESRF_clinic_spec_maxi_bins.txt"),
-    #     skiprows=1,
-    #     delimiter="\t",
-    # )
-    # source.energy.spectrum_energy = spectrum[:, 0] * eV
-    # source.energy.spectrum_weight = spectrum[:, 1]
+    source.energy.type = "spectrum_lines"
+    spectrum = np.loadtxt(
+        os.path.join(os.path.dirname(__file__), "data/ESRF_clinic_spec_maxi_bins.txt"),
+        skiprows=1,
+        delimiter="\t",
+    )
+    source.energy.spectrum_energy = spectrum[:, 0] * unit_spec_file
+    source.energy.spectrum_weight = spectrum[:, 1]
 
-    # Primary collimator
+    # plane = sim.add_volume("Box", "test_kill_actor")
+    # plane.mother = "vacuum_sec"
+    # plane.material = "Copper"
+    # plane.size = [20 * cm, 10 * cm, 1 * um]
+    # plane.translation = [0, 0, -10 * m]
+    # plane.color = [1, 0, 0, 1]
+
+    # # phase space test 1 to check spectrum
+    # if phsp:
+    #     plane = sim.add_volume("Box", "phsp_plane_test1")
+    #     plane.mother = "vacuum_sec"
+    #     plane.material = "Vacuum"
+    #     plane.size = [20 * cm, 10 * cm, 1 * um]
+    #     plane.translation = [0, 0, -9 * m]
+    #     plane.color = [0, 0, 1, 1]
+
+    #     # phaseSpace Actor
+    #     phsp_actor = sim.add_actor("PhaseSpaceActor", "phase_space_test_1")
+    #     phsp_actor.mother = plane.name
+    #     phsp_actor.attributes = [
+    #         "KineticEnergy",
+    #         "PrePositionLocal",
+    #         "ParticleName",
+    #         "PreDirectionLocal",
+    #     ]
+    #     phsp_actor.output = os.path.join(
+    #         os.path.dirname(__file__),
+    #         "output/phsp_test_1.root",
+    #     )
+    #     phsp_actor.debug = False
+
+    # # phase space test 2 to check spectrum
+    # if phsp:
+    #     plane = sim.add_volume("Box", "phsp_plane_test2")
+    #     plane.mother = "vacuum_sec"
+    #     plane.material = "Vacuum"
+    #     plane.size = [20 * cm, 10 * cm, 1 * um]
+    #     plane.translation = [0, 0, -11 * m]
+    #     plane.color = [0, 0, 1, 1]
+
+    #     # phaseSpace Actor
+    #     phsp_actor = sim.add_actor("PhaseSpaceActor", "phase_space_test_2")
+    #     phsp_actor.mother = plane.name
+    #     phsp_actor.attributes = [
+    #         "KineticEnergy",
+    #         "PrePositionLocal",
+    #         "ParticleName",
+    #         "PreDirectionLocal",
+    #     ]
+    #     phsp_actor.output = os.path.join(
+    #         os.path.dirname(__file__),
+    #         "output/phsp_test_2.root",
+    #     )
+    #     phsp_actor.debug = False
+
+    # test kill act vol
     # prim col width chosen to cover whole sec col width + margin on both sides
     prim_col_width = (beam_width + 2 * margin) * (d_source_prim_col / d_source_sec_col)
     prim_col_height = (beam_height + 2 * margin) * (
         d_source_prim_col / d_source_sec_col
     )
-    create_collimator(
-        sim,
+    print(f"prim_col_width = {prim_col_width}")
+    print(f"prim_col_height = {prim_col_height}")
+
+    y_top_stop_vol_1 = prim_col_height / 2 + 0.05 * m
+    width_top_stop_vol_1 = 0.2 * m + prim_col_width
+    top_stop_vol = create_box_vol(
+        "top_stop_vol_1",
         "vacuum_sec",
-        "pc_box",
-        "primary_collimator",
-        "primary_slit",
-        "Copper",
-        [0, 0, -10.75 * m],
-        [5 * cm, 5 * cm, 1 * cm],
-        [prim_col_width, prim_col_height, 1.1 * cm],
-        prim_col_cut,
-        prim_slit_cut,
+        [width_top_stop_vol_1, 0.1 * m, 1 * um],
+        [0, y_top_stop_vol_1, -10.75 * m],
+        "Vacuum",
+        [1, 0, 0, 1],
     )
+    sim.add_volume(top_stop_vol)
 
-    # Secondary collimator
-    create_collimator(
-        sim,
-        "world",
-        "sc_box",
-        "secondary_collimator",
-        "secondary_slit",
-        "msc_material",
-        [0, 0, -19.2 * m],
-        [8 * cm, 1 * cm, 8 * mm],
-        [beam_width, beam_height, 8.1 * mm],
-        sec_col_cut,
-        sec_slit_cut,
+    x_left_stop_vol = 0.05 * m + prim_col_width / 2
+    y_left_stop_vol = prim_col_height / 2 + (0.05 * m - prim_col_height)
+    print(f"y_left_stop_vol = {y_left_stop_vol}")
+    print(f"x_left_stop_vol = {x_left_stop_vol}")
+    left_stop_vol = create_box_vol(
+        "left_stop_vol_1",
+        "vacuum_sec",
+        [0.1 * m, 0.1 * m, 1 * um],
+        [x_left_stop_vol, -y_left_stop_vol, -10.75 * m],
+        "Vacuum",
+        [0, 0, 1, 1],
     )
+    sim.add_volume(left_stop_vol)
 
-    # Creates MSC
-    create_msc(
-        sim, -19.7 * m, 125, 400 * um, msc_cut, msc_slits_cut, offset_to_center=0
+    right_stop_vol = create_box_vol(
+        "right_stop_vol_1",
+        "vacuum_sec",
+        [0.1 * m, 0.1 * m, 1 * um],
+        [-x_left_stop_vol, -y_left_stop_vol, -10.75 * m],
+        "Vacuum",
+        [0, 0, 1, 1],
     )
+    sim.add_volume(right_stop_vol)
 
-    # kill actors
-    if kill_act:
-        kill_actor_pc = sim.add_actor("KillActor", "kill_actor_pc")
-        kill_actor_pc.mother = "primary_collimator"
-        kill_actor_sc = sim.add_actor("KillActor", "kill_actor_sc")
-        kill_actor_sc.mother = "secondary_collimator"
-        kill_actor_mlc = sim.add_actor("KillActor", "kill_actor_msc")
-        kill_actor_mlc.mother = "msc"
+    y_bot_stop_vol_1 = -(0.05 * m + prim_col_height / 2)
+    bot_stop_vol_1 = create_box_vol(
+        "bot_stop_vol_1",
+        "vacuum_sec",
+        [prim_col_width, 0.1 * m, 1 * um],
+        [0, y_bot_stop_vol_1, -10.75 * m],
+        "Vacuum",
+        [0, 1, 0, 1],
+    )
+    sim.add_volume(bot_stop_vol_1)
 
-    # virtual plane for phase space
-    if phsp:
-        plane = sim.add_volume("Box", "phsp_plane")
-        plane.mother = sim.world
-        plane.material = "G4_AIR"
-        plane.size = [20 * cm, 10 * cm, 1 * um]
-        plane.translation = [0, 0, -20 * m]
-        plane.color = [1, 0, 0, 1]
+    # # Primary collimator
+    # # prim col width chosen to cover whole sec col width + margin on both sides
+    # prim_col_width = (beam_width + 2 * margin) * (d_source_prim_col / d_source_sec_col)
+    # prim_col_height = (beam_height + 2 * margin) * (
+    #     d_source_prim_col / d_source_sec_col
+    # )
+    # create_collimator(
+    #     sim,
+    #     "vacuum_sec",
+    #     "pc_box",
+    #     "primary_collimator",
+    #     "primary_slit",
+    #     "TungstenCarbid",
+    #     [0, 0, -10.75 * m],
+    #     [5 * cm, 5 * cm, 1 * cm],
+    #     [prim_col_width, prim_col_height, 1.1 * cm],
+    #     "Vacuum",
+    #     prim_col_cut,
+    #     prim_slit_cut,
+    # )
 
-        # phaseSpace Actor
-        phsp_actor = sim.add_actor("PhaseSpaceActor", "PhaseSpace")
-        phsp_actor.mother = plane.name
-        phsp_actor.attributes = [
-            "KineticEnergy",
-            "Weight",
-            "PrePositionLocal",
-            "PreDirectionLocal",
-            "ParticleName",
-        ]
-        phsp_actor.output = os.path.join(
-            os.path.dirname(__file__),
-            f"output/phsp_esrf_line_{Decimal(N_PARTICLES):.3E}_events.root",
-        )
-        phsp_actor.debug = False
+    # # Secondary collimator
+    # create_collimator(
+    #     sim,
+    #     "world",
+    #     "sc_box",
+    #     "secondary_collimator",
+    #     "secondary_slit",
+    #     "msc_material",
+    #     [0, 0, -19.2 * m],
+    #     [8 * cm, 1 * cm, 8 * mm],
+    #     [beam_width, beam_height, 8.1 * mm],
+    #     sec_col_cut,
+    #     sec_slit_cut,
+    # )
+
+    # # Creates MSC
+    # create_msc(
+    #     sim, -19.7 * m, 125, 400 * um, msc_cut, msc_slits_cut, offset_to_center=0
+    # )
+
+    # killing of all particles in prim and sec col because only used for beam shaping
+    # kill_actor_pc = sim.add_actor("KillActor", "kill_actor_test")
+    # kill_actor_pc.mother = "test_kill_actor"
+
+    # kill_actor_pc = sim.add_actor("KillActor", "kill_actor_pc")
+    # kill_actor_sc = sim.add_actor("KillActor", "kill_actor_sc")
+    # kill_actor_sc.mother = "secondary_collimator"
+    # kill_actor_pc.mother = "primary_collimator"
+
+    # if kill_act:
+    #     kill_actor_pc = sim.add_actor("KillActor", "kill_actor_pc")
+    #     kill_actor_pc.mother = "primary_collimator"
+    #     kill_actor_sc = sim.add_actor("KillActor", "kill_actor_sc")
+    #     kill_actor_sc.mother = "secondary_collimator"
+    #     kill_actor_mlc = sim.add_actor("KillActor", "kill_actor_msc")
+    #     kill_actor_mlc.mother = "msc"
+
+    # # virtual plane for phase space
+    # if phsp:
+    #     plane = sim.add_volume("Box", "phsp_plane")
+    #     plane.mother = sim.world
+    #     plane.material = "G4_AIR"
+    #     plane.size = [20 * cm, 10 * cm, 1 * um]
+    #     plane.translation = [0, 0, -20 * m]
+    #     plane.color = [1, 0, 0, 1]
+
+    #     # phaseSpace Actor
+    #     phsp_actor = sim.add_actor("PhaseSpaceActor", "PhaseSpace")
+    #     phsp_actor.mother = plane.name
+    #     phsp_actor.attributes = [
+    #         "KineticEnergy",
+    #         "Weight",
+    #         "PrePositionLocal",
+    #         "PreDirectionLocal",
+    #         "ParticleName",
+    #     ]
+    #     phsp_actor.output = os.path.join(
+    #         os.path.dirname(__file__),
+    #         f"output/phsp_esrf_line_{Decimal(N_PARTICLES):.3E}_events.root",
+    #     )
+    #     phsp_actor.debug = False
 
     # phys
     sim.physics_manager.physics_list_name = physics_list
