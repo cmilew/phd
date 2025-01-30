5254388a08054f9f9061173407f44cc70439a2ba
cmilew <milewskicandice@gmail.com>
2025-01-30 16:18:04 +0100

    only central strip plotted for ESRF setup

--- codes_python/simu_mc/analysis/mu_analysis_MC.py
+++ codes_python/simu_mc/analysis/mu_analysis_MC.py
@@ -3,15 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import pandas as pd
-from decimal import Decimal
 import sys
 
 
-def get_excel_data(mc_data_file, mc_ws_name, start_l, start_col, n_l, n_c):
+def get_excel_data(data_file, ws_name, start_l, start_col, n_l, n_c):
     """gets normalization values from excel file calculated on step 0 of step phantom
     measurements"""
-    wb_res = load_workbook(mc_data_file)
-    ws = wb_res[mc_ws_name]
+    wb_res = load_workbook(data_file)
+    ws = wb_res[ws_name]
     excel_data = []
     for line in range(n_l):
         for col in range(n_c):
@@ -41,11 +40,11 @@ def calc_mu(step_thickness, edep):
     return mu, mu_uncertain
 
 
-def fill_excel(excel_path, mc_ws_name, data_to_fill, start_l, start_col):
+def fill_excel(excel_path, ws_name, data_to_fill, start_l, start_col):
     wb_res = load_workbook(excel_path)
-    ws = wb_res[mc_ws_name]
+    ws = wb_res[ws_name]
     for i in range(len(data_to_fill)):
-        ws.cell(row=start_l + i, column=start_col, value=data_to_fill[i])
+        ws.cell(row=start_l + i * 2, column=start_col, value=data_to_fill[i])
     wb_res.save(excel_path)
 
 
@@ -89,29 +88,29 @@ def get_fit_expo_data(x_data, y_data):
 n_strips = 136
 step_thickness = np.array([0, 1, 2, 3, 4, 6])
 step_phant_mat = "RW3"
-mc_data_file = r"C:\Users\milewski\OneDrive - Université Grenoble Alpes\these\papiers\caracterisation_detecteur_153_voies\simulations_MC\results_step_phantom.xlsx"
-mes_data_file = r"C:\Users\milewski\OneDrive - Université Grenoble Alpes\these\papiers\caracterisation_detecteur_153_voies\step_phantom\param_et_resultats.xlsx"
-mc_ws_name = "ESRF_poly"
-mes_ws_name = "mean_results"
-start_l_mc = 8
-start_c_mc = 2
-start_l_mes = 8
-start_c_mes = 6
+data_file = r"C:\Users\milewski\OneDrive - Université Grenoble Alpes\these\papiers\caracterisation_detecteur_153_voies\simulations_MC\results_step_phantom.xlsx"
+ws_name = "ESRF_poly"
+start_l_mc = 33
+start_c_mc = 31
+start_l_mes = 33
+start_c_mes = 30
 mu_NIST = 0.1616615
+fill_excel_bool = False
+fontsize_val = 20
 # ########################################
 
 # get edep of strips obtained from MC simu from excel file
-df = pd.read_excel(mc_data_file, sheet_name=mc_ws_name)
-subset_df = df.iloc[
-    start_l_mc : start_l_mc + n_strips, start_c_mc : start_c_mc + len(step_thickness)
-]
+df = pd.read_excel(data_file, sheet_name=ws_name)
+subset_df = df.iloc[8 : 8 + n_strips, 2 : 2 + len(step_thickness)]
 subset_df_uncertain = df.iloc[
-    start_l_mc : start_l_mc + n_strips,
-    start_c_mc + 7 : start_c_mc + 7 + len(step_thickness) * 2,
+    8 : 8 + n_strips,
+    9 : 9 + len(step_thickness) * 2,
 ]
 all_edep_mc = subset_df.to_numpy()
 all_uncertain_mc = subset_df_uncertain.to_numpy()
 
+all_edep_mc = all_edep_mc.astype(float)
+all_uncertain_mc = all_uncertain_mc.astype(float)
 
 # retrieves only statistical uncertainties
 stat_uncertain_mc = all_uncertain_mc[:, 0::2]
@@ -120,33 +119,29 @@ stat_uncertain_mc = all_uncertain_mc[:, 0::2]
 edep_mc = np.array(all_edep_mc[1::2], dtype=float)
 uncertain_mc = np.array(stat_uncertain_mc[1::2], dtype=float)
 
-# normalizes edep by the value at step 0 and propagates uncertainties
-normal_edep_mc, normal_uncertain_mc = generate_normal_matrix(edep_mc, uncertain_mc)
+# calc mu for these strips
+strip_mu_mc = []
+uncertain_mu_fit_mc = []
+for strip_edep in edep_mc:
+    mu, uncertain_mu = calc_mu(step_thickness, strip_edep)
+    strip_mu_mc.append(mu)
+    uncertain_mu_fit_mc.append(uncertain_mu)
+if fill_excel_bool:
+    fill_excel(data_file, "test", strip_mu_mc, 2, 2)
+    fill_excel(data_file, "test", uncertain_mu_fit_mc, 2, 3)
 
-# calc mean value and propagates uncertainties
-mean_edep_mc = np.mean(normal_edep_mc, axis=0)
-mean_uncertain_mc = (1 / len(normal_uncertain_mc)) * np.sqrt(
-    np.array(np.sum(normal_uncertain_mc**2, axis=0), dtype=np.float64)
-)
 
-# get measurements from excel file
-edep_mes = get_excel_data(
-    mes_data_file, mes_ws_name, start_l_mes, start_c_mes, len(step_thickness), 1
-)
-uncertain_mes = get_excel_data(
-    mes_data_file, mes_ws_name, start_l_mes, start_c_mes + 1, len(step_thickness), 1
-)
+# get measurements and mc simul of strip 86 (= center strip of 8 diamonds) for plot
+edep_mes_s86 = get_excel_data(data_file, ws_name, 10, 37, len(step_thickness), 1)
+uncertain_mes_s86 = get_excel_data(data_file, ws_name, 19, 37, len(step_thickness), 1)
 
-# exponential fit to retrieve linear att coeff
-mu_mc, uncertain_mu_mc = calc_mu(step_thickness, mean_edep_mc)
-mu_mes, uncertain_mu_mes = calc_mu(step_thickness, edep_mes)
+edep_mc_s86 = get_excel_data(data_file, ws_name, 10, 38, len(step_thickness), 1)
+uncertain_mc_s86 = get_excel_data(data_file, ws_name, 19, 38, len(step_thickness), 1)
 
-# print(f"mu_mc {mu_mc:.3f} +/- {Decimal(uncertain_mu_mc):.2E}")
-# sys.exit()
 
-# theoratical curve
-x_theo = np.arange(0, 5, 0.05, dtype=float)
-y_theo = np.exp(-mu_NIST * x_theo)
+# # theoratical curve
+# x_theo = np.arange(0, 5, 0.05, dtype=float)
+# y_theo = np.exp(-mu_NIST * x_theo)
 
 # # fit exponential model to mean of in beam sum edep
 # mean_mu_mc, mean_mu_uncertain_mc, x_fit_mc, y_fit_mc = get_fit_expo_data(
@@ -155,43 +150,48 @@ y_theo = np.exp(-mu_NIST * x_theo)
 # mu_mes, mu_mes_uncertain, x_fit_mes, y_fit_mes = get_fit_expo_data(step_thickness, mes)
 
 # plt.plot(x_theo, y_theo, label="theory (NIST)", linestyle="--", color="black")
-plt.scatter(step_thickness, mean_edep_mc, label="MC simulations")
+
+# strip 86 = 42 in strip_mu_mc because takes only microbeam lines
+mu_mc_s86 = strip_mu_mc[42]
+mu_uncertain_mc_s86 = uncertain_mu_fit_mc[42]
+
+mu_mes_s86, uncertain_mu_mes_s86 = calc_mu(step_thickness, edep_mes_s86)
+
 plt.errorbar(
     step_thickness,
-    mean_edep_mc,
-    yerr=mean_uncertain_mc,
+    edep_mes_s86,
+    yerr=uncertain_mes_s86,
     fmt="o",
     capsize=5,
     color="blue",
+    label="experimental",
 )
-plt.scatter(step_thickness, edep_mes, marker="^", label="experimental")
 plt.errorbar(
-    step_thickness, edep_mes, yerr=uncertain_mes, fmt="^", capsize=5, color="orange"
+    step_thickness,
+    edep_mc_s86,
+    yerr=uncertain_mc_s86,
+    fmt="^",
+    capsize=5,
+    color="orange",
+    label="MC simulations",
 )
 plt.plot(
     [],
     [],
     color="white",
-    label=f"$\mu$(RW3) MC = {mu_mc:.3f} $\pm$ {round(uncertain_mu_mc, 3)} cm\u207b\u00b9",
+    label=f"$\mu$ MC = {mu_mc_s86:.3f} $\pm$ {round(mu_uncertain_mc_s86, 3)} cm\u207b\u00b9",
 )
 plt.plot(
     [],
     [],
     color="white",
-    label=f"$\mu$(RW3) exp = {mu_mes:.3f} $\pm$ {round(uncertain_mu_mes, 3)} cm\u207b\u00b9",
+    label=f"$\mu$ exp = {mu_mes_s86:.3f} $\pm$ {round(uncertain_mu_mes_s86, 3)} cm\u207b\u00b9",
 )
-# plt.plot(
-#     [],
-#     [],
-#     color="white",
-#     label=f"$\mu$ NIST = {mu_NIST:.3f} cm\u207b\u00b9",
-# )
-# plt.title(
-#     "Comparison between attenuation coefficient $\mu$ measured at ANSTO and simulated by MC",
-#     fontsize=15,
-# )
+
 x_label = step_phant_mat + " thickness (cm)"
-plt.xlabel(x_label, fontsize=15)
-plt.ylabel("Response / Response step 0", fontsize=15)
-plt.legend()
+plt.xlabel(x_label, fontsize=fontsize_val)
+plt.ylabel("Response / Response step 0", fontsize=fontsize_val)
+plt.tick_params(axis="x", labelsize=fontsize_val)
+plt.tick_params(axis="y", labelsize=fontsize_val)
+plt.legend(fontsize=fontsize_val)
 plt.show()
